0889e8b54603043deb3f23207dfccf1bda4234af
fenomelini <felipe@nomelini.com.br>
2026-03-13 16:03:46 -0300

    fix: update version number to 0.1.7-alpha in manifest.json and header
    Closes #53

--- src/00_header.lua
+++ src/00_header.lua
@@ -1,7 +1,7 @@
 -- Balatro Odyssey
 -- A mod adding 1000 unique Jokers
 -- Author: fenomelini
--- Version: 0.1.4-alpha
+-- Version: 0.1.7-alpha
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
