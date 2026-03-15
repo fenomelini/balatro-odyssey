3f46693089ccd942764a557140b711f454e2fb79
fenomelini <felipe@nomelini.com.br>
2026-03-15 20:49:47 -0300

    release: bump version to 0.1.9-alpha

--- src/00_header.lua
+++ src/00_header.lua
@@ -1,7 +1,7 @@
 -- Balatro Odyssey
 -- A mod adding 1000 unique Jokers
 -- Author: fenomelini
--- Version: 0.1.8-alpha
+-- Version: 0.1.9-alpha
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
